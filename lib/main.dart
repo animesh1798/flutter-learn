9ad545f49e683d718ca7830c1991eb1f5fe83362
animeshltts <animesh.srivastava@ltts.com>
2022-06-27 13:46:42 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:demo/firebase_options.dart';
+import 'package:demo/views/login_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page"),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user!.emailVerified) {
+                  // ignore: avoid_print
+                  print('You are verified');
+                } else {
+                  // ignore: avoid_print
+                  print('Not verfied');
+                }
+                return const Text("Done");
+              default:
+                return const Text("Loading...");
+            }
+          },
+        ));
+  }
+}
